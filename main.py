edf9b73f92e7a10068069f98dd963fe5d5e1da35
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-03-22 16:21:27 +0100

    Potential design of main game class

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ Uit, Institute of Computer Science, 2022
 Clone of the classic Amiga game, Mayhem. Written as an assignment in Inf-1400 Object-oriented programming
 """
 
+class Mayhem:
+    def __init__(self, *argv):
+        pass
+
+    def EventHandler(self):
+        pass
+
+    def Update(self):
+        pass
+
+    def Draw(self):
+        pass
+
+    def Main(self):
+        pass
