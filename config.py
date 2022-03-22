dc739a847a3de5260cf7394c7fd74268d9616eff
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-03-22 17:58:31 +0100

    Updated config file

---
+++ config.py
@@ -0,0 +1,6 @@
+"""
+@authors: Daniel Elisabethsønn Antonsen and Håkon Alfred Bakker
+UiT, Institute of Computer Science
+
+Module containing all constants used in the game.
+"""
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-@author: Daniel Elisabethsønn Antonsen and Håkon Alfred Bakker
+@authors: Daniel Elisabethsønn Antonsen and Håkon Alfred Bakker
 Uit, Institute of Computer Science, 2022
 
 Clone of the classic Amiga game, Mayhem. Written as an assignment in Inf-1400 Object-oriented programming
