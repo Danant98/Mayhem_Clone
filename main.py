18268cf7bde3ec55622ee01dd2f0a34a6f3dbaf2
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-03-22 16:08:52 +0100

    Added main python file

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+@author: Daniel Elisabethsønn Antonsen
+Uit, Institute of Computer Science, 2022
+
+Clone of the classic Amiga game, Mayhem. Written as an assignment in Inf-1400 Object-oriented programming
+"""
+
