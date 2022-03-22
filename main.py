a549ace173bf411eed0a434bf547af655f7b44be
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-03-22 19:01:34 +0100

    Import pygame

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ Uit, Institute of Computer Science, 2022
 
 Clone of the classic Amiga game, Mayhem. Written as an assignment in Inf-1400 Object-oriented programming.
 """
-
+# Importing modules and libratries
+import pygame
+#
 class Mayhem:
     def __init__(self, *argv):
         pass
