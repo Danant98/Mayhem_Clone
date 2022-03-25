541a8358b1cd560898e4bb1f8386eee2d4abb402
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-03-25 14:45:46 +0100

    Added fullscreen

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ Module containing all constants used in the game.
 # Defining height and widht of the screen window
 WIDHT, HEIGHT = 1000, 800
 # Color for the background of the screen
-BLACK = "#000000"
+BLACK = "#0000CD"
 
