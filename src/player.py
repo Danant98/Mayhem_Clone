4d549b58258d17e16288d6f510e036af0f30c14a
Danant98 <danieleantonsen@gmail.com>
2022-03-30 14:15:12 +0200

    Updated functions

--- src/player.py
+++ src/player.py
@@ -12,18 +12,20 @@ class Player(pygame.sprite.Sprite):
     """
     Class object to represent the players
     """
-    def __init__(self, x, y, color, SCREEN):
+    def __init__(self, x, y, COLOR, SCREEN):
         # Constructor from parent class 
         super().__init__()
         self.SCREEN = SCREEN
-        self.color = color
+        self.COLOR = COLOR
         self.fuel = Config.maxFuel
-        self.gravity = Config.GRAVITY
-        self.image = pygame.Surface([100, 100], pygame.SRCALPHA)
+        self.GRAVITY = Config.GRAVITY
+        self.image = pygame.Surface([100, 100], pygame.SRCALPHA, 32)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        pygame.draw.polygon(self.image, self.color, ([(self.rect.x + 50, self.rect.y),(self.rect.x, self.rect.y + 100), (self.rect.x + 100, self.rect.y + 100)]))
+        pygame.draw.polygon(self.image, self.COLOR, ([(self.rect.x + 50, self.rect.y),
+                                                    (self.rect.x, self.rect.y + 100),
+                                                    (self.rect.x + 100, self.rect.y + 100)]))
 
     #def draw(self):
      #   pygame.draw.polygon(self.image, self.color, ([(self.rect.x + 50, self.rect.y),(self.rect.x, self.rect.y + 100), (self.rect.x + 100, self.rect.y + 100)]))
