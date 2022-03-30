0e07443fff327181f94febfc6c9fb907c0a4b28c
bakker123 <hakonbakker@gmail.com>
2022-03-30 14:49:33 +0200

    Draw triangle

--- src/player.py
+++ src/player.py
@@ -12,18 +12,21 @@ class Player(pygame.sprite.Sprite):
     """
     Class object to represent the players
     """
-    def __init__(self, x, y, color, SCREEN):
+    def __init__(self, x, y, COLOR, SCREEN):
         # Constructor from parent class 
         super().__init__()
         self.SCREEN = SCREEN
-        self.color = color
+        self.COLOR = COLOR
         self.fuel = Config.maxFuel
-        self.gravity = Config.GRAVITY
-        self.image = pygame.Surface([100, 100], pygame.SRCALPHA)
+        self.GRAVITY = Config.GRAVITY
+        self.image = pygame.Surface([Config.playerHitboxW, Config.playerHitboxH], pygame.SRCALPHA)
+        #pygame.draw.circle(self.image, (255, 255, 255), (Config.playerHitboxW / 2, Config.playerHitboxW / 2), Config.playerHitboxW / 2 )
+        pygame.draw.polygon(self.image, self.COLOR, [(Config.platformX, Config.platformY),
+                                                    (Config.platformX, Config.platformY + 100),
+                                                    (Config.platformX + 100, Config.platformY + 50)])
         self.rect = self.image.get_rect()
-        self.rect.x = x
+        self.rect.x = x - (Config.playerHitboxW / 2)
         self.rect.y = y
-        pygame.draw.polygon(self.image, self.color, ([(self.rect.x + 50, self.rect.y),(self.rect.x, self.rect.y + 100), (self.rect.x + 100, self.rect.y + 100)]))
 
     #def draw(self):
      #   pygame.draw.polygon(self.image, self.color, ([(self.rect.x + 50, self.rect.y),(self.rect.x, self.rect.y + 100), (self.rect.x + 100, self.rect.y + 100)]))
